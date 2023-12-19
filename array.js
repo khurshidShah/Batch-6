a3ccbabd533f1fb5b58585adc0d0510c174f4803
khursheed <you@example.com>
2023-12-18 22:10:55 -0800

    basic array method

---
+++ array.js
@@ -0,0 +1,24 @@
+// array method
+// index reverse
+let array = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+let arrayRev = array.reverse();
+console.log(array);
+// array concat
+const per = ['name', 'class', 'section'];
+const per1 = ['roll_no', 'phone_number', 'contact'];
+const bothInfo = per.concat(per1);
+console.log(bothInfo);
+
+// array toString
+let detail = ['03446055207'];
+console.log(typeof (detail));
+const detailtoString = detail.toLocaleString()
+console.log(typeof (detailtoString))
+console.log(detailtoString);
+// array push method
+
+const array2 = ['how are you a person'];
+const addarray2 = array2.push('guys');
+console.log(array2)
+
+
