ca6c03cfdae4a82d28c8bb8e2a3f983554759836
khursheed <you@example.com>
2023-12-18 21:53:51 -0800

    varible method

--- varible.js
+++ varible.js
@@ -1,1 +1,25 @@
-console.log('hello wrold')
+let person1 = "hello";
+console.log(person1);
+// var with Object
+
+var personInfo = {
+    name: 'khurshid alam',
+    class: 'web tech',
+    email: "apocho@gmail.com",
+}
+console.log(personInfo);
+personInfo.email = "@lala10@gmail.com";
+// array method with unshift
+let arr = ['1', '2', '3', '4',];
+let arr2 = arr.unshift('2');
+console.log(arr);
+
+console.log(ab);
+var ab = 'varible hoisting problem';
+
+// varible with arrow function
+const person1Info = () => {
+    console.log(person1Info);
+
+}
+person1Info('hello');
